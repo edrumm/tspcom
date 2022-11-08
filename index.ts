9de858a6d7b5a1c3d64dd107405a26450f878530
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2022-11-08 20:48:55 +0000

    init and install packages
    installed jest, eslint
    setup config for eslint, ts, jest, npm

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Under construction');
+type JsonValue = null
+    | number
+    | string
+    | boolean
+    | JsonValue[]
+    | {[key: string]: JsonValue};
+
+function run(parser: Function, input: string): void {} // TODO: add return type
+
+const parseChar = (): void => {}; // TODO: add return type
+
+// something like this...
+// run(parseChar, 'TypeScript') => ('ypeScript', 'T')
